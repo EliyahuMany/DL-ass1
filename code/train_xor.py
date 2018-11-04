17548178f436710bc61020d00781f41cf523fe66
yifrach <33986278+yifrach@users.noreply.github.com>
2018-11-04 22:22:12 +0200

    Update train_xor.py
    test.pred in data directory

--- code/train_xor.py
+++ code/train_xor.py
@@ -69,6 +69,19 @@ def create_classifier(in_dim, hid_dim, out_dim):
     return [W, b, W_tag, b_tag]
 
 
+def test(test_data, params):
+    prediction_file= open("../data/test.pred", 'w')
+    for label, features in test_data:
+        x = feats_to_vec(features)  # convert features to a vector.
+        pred = ml.predict(x, params)
+        for key, val in ut.L2I.items():
+            if val == pred:
+                label = key
+                break
+        prediction_file.write(str(label) + "\n")
+    prediction_file.close()
+
+           
 if __name__ == '__main__':
     # YOUR CODE HERE
     # write code to load the train and dev sets, set up whatever you need,
@@ -81,3 +94,4 @@ if __name__ == '__main__':
 
     params = create_classifier(in_dim , hidden, out_dim)
     trained_params = train_classifier(xor.data, num_iterations, learning_rate, params)
+    test(ut.TRAIN,trained_params)
